b62181b4816abed764865bc8b8703d191dbeb6a2
Scott Young <youngscottj@gmail.com>
2019-08-29 20:13:12 -0600

    build out artist controller and routes

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,13 +1,43 @@
 class ArtistsController < ApplicationController
-  def index
-  end
+  before_action :set_artist, only: [:show, :edit, :update, :destroy]
 
-  def new
+  def index
+    @artists = Artist.all
   end
 
   def show
   end
 
-  def edit
+  def new
+    @artist = Artist.new
   end
+
+  def create
+    @artist = Artist.new(artist_params)
+    @song.save ? (redirect_to artist_path) : (render :new)
+  end
+
+  def edit
+    render partial 'form'
+  end
+
+  def update
+    @artist.update(artist_params) ? (redirect_to artist_path) : (render :edit)
+  end
+
+  def destroy
+    @artist.destroy
+    redirect_to artist_path
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name, :rank)
+  end
+
+  def set_artist
+  @artist = Artist.find(params[:id])
+end
+
 end
